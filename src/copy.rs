3a1916ac0a45a9d28202f54f156123f9e1e0ef97
John Baublitz <john.m.baublitz@gmail.com>
2018-07-21 19:16:14 -0400

    Simplify Cursor reader/writer types to mirror Read/Write impls in the standard lib and add better solution for truncating zeroed bytes when yielding serialized packets

--- src/copy.rs
+++ src/copy.rs
@@ -1,48 +1,58 @@
 use std::io::{self,Cursor,Read,Write};
 
-pub enum StreamReadBuffer<'a> {
-    Growable(Cursor<Vec<u8>>),
-    Static(Cursor<&'a [u8]>),
-}
+pub struct StreamReadBuffer<T>(Cursor<T>);
 
-impl<'a> Read for StreamReadBuffer<'a> {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
-        match *self {
-            StreamReadBuffer::Growable(ref mut b) => b.read(buf),
-            StreamReadBuffer::Static(ref mut b) => b.read(buf),
-        }
+impl<T> Read for StreamReadBuffer<T> where T: AsRef<[u8]> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.read(buf)
     }
 }
 
-impl<'a> AsRef<[u8]> for StreamReadBuffer<'a> {
+impl<T> AsRef<[u8]> for StreamReadBuffer<T> where T: AsRef<[u8]> {
     fn as_ref(&self) -> &[u8] {
-        match *self {
-            StreamReadBuffer::Growable(ref b) => b.get_ref().as_slice(),
-            StreamReadBuffer::Static(ref b) => b.get_ref(),
-        }
-    }
-}
-
-impl<'a> From<StreamWriteBuffer<'a>> for StreamReadBuffer<'a> {
-    fn from(v: StreamWriteBuffer<'a>) -> Self {
-        match v {
-            StreamWriteBuffer::Growable(b) => StreamReadBuffer::Growable(b),
-            StreamWriteBuffer::Static(b) => StreamReadBuffer::Static(Cursor::new(b.into_inner())),
-        }
+        self.0.get_ref().as_ref()
     }
 }
 
 pub enum StreamWriteBuffer<'a> {
-    Growable(Cursor<Vec<u8>>),
-    Static(Cursor<&'a mut [u8]>),
+    Growable(Cursor<Vec<u8>>, usize),
+    Sized(Cursor<&'a mut [u8]>, usize),
+}
+
+impl<'a> StreamWriteBuffer<'a> {
+    pub fn new_growable(cap: Option<usize>) -> Self {
+        match cap {
+            Some(sz) => StreamWriteBuffer::Growable(Cursor::new(Vec::with_capacity(sz)), 0),
+            None => StreamWriteBuffer::Growable(Cursor::new(vec![]), 0),
+        }
+    }
+
+    pub fn new_sized(buf: &'a mut [u8]) -> Self {
+        StreamWriteBuffer::Sized(Cursor::new(buf), 0)
+    }
+
+    pub fn yield_buffer(&self) -> &[u8] {
+        match *self {
+            StreamWriteBuffer::Growable(ref c, ref len) => &c.get_ref()[0..*len],
+            StreamWriteBuffer::Sized(ref c, ref len) => &c.get_ref()[0..*len],
+        }
+    }
 }
 
 impl<'a> Write for StreamWriteBuffer<'a> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        match *self {
-            StreamWriteBuffer::Growable(ref mut b) => b.write(buf),
-            StreamWriteBuffer::Static(ref mut b) => b.write(buf),
-        }
+        Ok(match *self {
+            StreamWriteBuffer::Growable(ref mut c, ref mut len) => {
+                let bytes_read = c.write(buf)?;
+                *len += bytes_read;
+                bytes_read
+            },
+            StreamWriteBuffer::Sized(ref mut c, ref mut len) => {
+                let bytes_read = c.write(buf)?;
+                *len += bytes_read;
+                bytes_read
+            },
+        })
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -50,11 +60,20 @@ impl<'a> Write for StreamWriteBuffer<'a> {
     }
 }
 
+impl<'a> AsRef<[u8]> for StreamWriteBuffer<'a> {
+    fn as_ref(&self) -> &[u8] {
+        match *self {
+            StreamWriteBuffer::Growable(ref c, _) => c.get_ref().as_ref(),
+            StreamWriteBuffer::Sized(ref c, _) => c.get_ref().as_ref(),
+        }
+    }
+}
+
 impl<'a> AsMut<[u8]> for StreamWriteBuffer<'a> {
     fn as_mut(&mut self) -> &mut [u8] {
         match *self {
-            StreamWriteBuffer::Growable(ref mut b) => b.get_mut().as_mut_slice(),
-            StreamWriteBuffer::Static(ref mut b) => b.get_mut(),
+            StreamWriteBuffer::Growable(ref mut c, _) => c.get_mut().as_mut(),
+            StreamWriteBuffer::Sized(ref mut c, _) => c.get_mut().as_mut(),
         }
     }
 }
