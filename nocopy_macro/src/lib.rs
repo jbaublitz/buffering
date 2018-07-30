2b260370e9a94e6a4fba0236074e4051195cef6b
John Baublitz <john.m.baublitz@gmail.com>
2018-07-29 21:31:46 -0400

    Add field attributes for endian specification - defaults to native endian

--- nocopy_macro/src/lib.rs
+++ nocopy_macro/src/lib.rs
@@ -6,19 +6,14 @@ extern crate syn;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(NoCopy)]
-pub fn no_copy(input: TokenStream) -> TokenStream {
-    enum Endian {
-        Big,
-        Little,
-    }
+enum Endian {
+    Big,
+    Little,
+    Default,
+}
 
-    let ast: syn::DeriveInput = syn::parse(input).expect("Failed to parse input");
-
-    let name = ast.ident;
-    let mut attrname = syn::Ident::new(format!("{}Buffer", name).as_str(), proc_macro2::Span::call_site());
-    let mut endian = Endian::Big;
-    for attr in ast.attrs {
+fn extract_attr_name(ast: &syn::DeriveInput, attrname: &mut syn::Ident) {
+    for attr in &ast.attrs {
         match attr.style {
             syn::AttrStyle::Outer => (),
             _ => panic!("Only outer attributes allowed here"),
@@ -27,19 +22,85 @@ pub fn no_copy(input: TokenStream) -> TokenStream {
         match attrnamemeta {
             Some(syn::Meta::NameValue(syn::MetaNameValue { ident, eq_token: _, lit: syn::Lit::Str(s) })) => {
                 if ident == syn::Ident::new("name", proc_macro2::Span::call_site()) {
-                    attrname = syn::Ident::new(s.value().as_str(), proc_macro2::Span::call_site());
+                    *attrname = syn::Ident::new(s.value().as_str(), proc_macro2::Span::call_site());
                 }
+            },
+            _ => panic!("Outer attribute must be in the form #[key = \"value\"]"),
+        };
+    }
+}
+
+fn match_endian(named_field: &syn::Field) -> proc_macro2::TokenStream {
+    let ident = &named_field.ident;
+    let get_ident = syn::Ident::new(format!("get_{}", named_field.ident.as_ref().expect("All fields must be named")).as_str(), proc_macro2::Span::call_site());
+    let set_ident = syn::Ident::new(format!("set_{}", named_field.ident.as_ref().expect("All fields must be named")).as_str(), proc_macro2::Span::call_site());
+    let ty = &named_field.ty;
+
+    let mut endian = Endian::Default;
+    for attr in &named_field.attrs {
+        match attr.style {
+            syn::AttrStyle::Outer => (),
+            _ => panic!("Only outer attributes allowed here"),
+        };
+        let attrnamemeta = attr.interpret_meta();
+        match attrnamemeta {
+            Some(syn::Meta::NameValue(syn::MetaNameValue { ident, eq_token: _, lit: syn::Lit::Str(s) })) => {
                 if ident == syn::Ident::new("endian", proc_macro2::Span::call_site()) {
                     match s.value().as_str() {
-                        "big" => (),
+                        "big" => { endian = Endian::Big; },
                         "little" => { endian = Endian::Little; },
                         _ => panic!("Unrecognized \"endian\" option"),
                     }
                 }
             },
             _ => panic!("Outer attribute must be in the form #[key = \"value\"]"),
-        };
+        }
     }
+
+    match endian {
+        Endian::Big => {
+            quote! {
+                pub fn #get_ident(&self) -> #ty {
+                    unsafe { #ty::from_be(self.structure.#ident) }
+                }
+
+                pub fn #set_ident(&mut self, v: #ty) {
+                    unsafe { self.structure.#ident = v.to_be(); }
+                }
+            }
+        },
+        Endian::Little => {
+            quote! {
+                pub fn #get_ident(&self) -> #ty {
+                    unsafe { #ty::from_le(self.structure.#ident) }
+                }
+
+                pub fn #set_ident(&mut self, v: #ty) {
+                    unsafe { self.structure.#ident = v.to_le(); }
+                }
+            }
+        },
+        Endian::Default => {
+            quote! {
+                pub fn #get_ident(&self) -> #ty {
+                    unsafe { self.structure.#ident }
+                }
+
+                pub fn #set_ident(&mut self, v: #ty) {
+                    unsafe { self.structure.#ident = v; }
+                }
+            }
+        },
+    }
+}
+
+#[proc_macro_derive(NoCopy)]
+pub fn no_copy(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).expect("Failed to parse input");
+
+    let name = &ast.ident;
+    let mut attrname = syn::Ident::new(format!("{}Buffer", name).as_str(), proc_macro2::Span::call_site());
+    extract_attr_name(&ast, &mut attrname);
 
     let fields = match ast.data {
         syn::Data::Struct(structure) => structure.fields,
@@ -52,26 +113,10 @@ pub fn no_copy(input: TokenStream) -> TokenStream {
 
     let mut funcs_vec = Vec::new();
     for named_field in field_pairs {
-        let ident = &named_field.ident;
-        let get_ident = syn::Ident::new(format!("get_{}", named_field.ident.as_ref().expect("All fields must be named")).as_str(), proc_macro2::Span::call_site());
-        let set_ident = syn::Ident::new(format!("set_{}", named_field.ident.as_ref().expect("All fields must be named")).as_str(), proc_macro2::Span::call_site());
-        let ty = named_field.ty;
-        funcs_vec.push(
-            quote! {
-                pub fn #get_ident(&self) -> #ty {
-                    unsafe { self.structure.#ident }
-                }
-
-                pub fn #set_ident(&mut self, v: #ty) {
-                    unsafe { self.structure.#ident = v; }
-                }
-            }
-        );
+        funcs_vec.push(match_endian(&named_field));
     }
 
     let tokens = quote! {
-        use std::mem;
-
         #[derive(Copy,Clone)]
         #[repr(C)]
         pub union #attrname {
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ pub use copy::*;
 
 #[cfg(test)]
 mod test {
+    use std::mem;
+
     #[test]
     fn test_proc_macro() {
         #[derive(Copy,Clone,NoCopy)]
@@ -20,5 +22,25 @@ mod test {
         let mut tb = TestBufferThing::new_buffer([0]);
         tb.set_test(5);
         assert_eq!(tb.as_buffer(), [5]);
+
+        #[derive(Copy,Clone,NoCopy)]
+        #[name = "TestBufferThingTwo"]
+        pub struct TestTwo {
+            #[endian = "big"]
+            test: u16,
+        }
+
+        let tb = TestBufferThingTwo::new_buffer([0, 5]);
+        assert_eq!(tb.get_test(), 5);
+
+        #[derive(Copy,Clone,NoCopy)]
+        #[name = "TestBufferThingThree"]
+        pub struct TestThree {
+            #[endian = "little"]
+            test: u16,
+        }
+
+        let tb = TestBufferThingThree::new_buffer([5, 0]);
+        assert_eq!(tb.get_test(), 5);
     }
 }
