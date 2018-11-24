93cc46d4bab2caaf057d6457ddede783bac7ccd4
John Baublitz <john.m.baublitz@gmail.com>
2018-11-24 17:09:36 -0500

    Remove some of the methods for use in syscalls in favor of better encapsulation in neli

--- src/copy.rs
+++ src/copy.rs
@@ -21,56 +21,29 @@ impl<T> AsRef<[u8]> for StreamReadBuffer<T> where T: AsRef<[u8]> {
 }
 
 pub enum StreamWriteBuffer<'a> {
-    Growable(Cursor<Vec<u8>>, usize),
-    Sized(Cursor<&'a mut [u8]>, usize),
+    Growable(Cursor<Vec<u8>>),
+    Sized(Cursor<&'a mut [u8]>),
 }
 
 impl<'a> StreamWriteBuffer<'a> {
     pub fn new_growable(size: Option<usize>) -> Self {
         match size {
-            Some(sz) => StreamWriteBuffer::Growable(Cursor::new(vec![0; sz]), 0),
-            None => StreamWriteBuffer::Growable(Cursor::new(vec![]), 0),
+            Some(sz) => StreamWriteBuffer::Growable(Cursor::new(vec![0; sz])),
+            None => StreamWriteBuffer::Growable(Cursor::new(vec![])),
         }
     }
 
     pub fn new_sized(buf: &'a mut [u8]) -> Self {
-        StreamWriteBuffer::Sized(Cursor::new(buf), 0)
-    }
-
-    pub fn set_bytes_written(&mut self, size: usize) -> io::Result<()> {
-        match *self {
-            StreamWriteBuffer::Growable(ref c, ref mut len) => {
-                if c.get_ref().len() < *len + size {
-                    return Err(io::Error::from(io::ErrorKind::InvalidInput));
-                }
-                *len += size;
-                Ok(())
-            },
-            StreamWriteBuffer::Sized(ref c, ref mut len) => {
-                if c.get_ref().len() < *len + size {
-                    return Err(io::Error::from(io::ErrorKind::InvalidInput));
-                }
-                *len += size;
-                Ok(())
-            },
-        }
+        StreamWriteBuffer::Sized(Cursor::new(buf))
     }
 }
 
 impl<'a> Write for StreamWriteBuffer<'a> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        Ok(match *self {
-            StreamWriteBuffer::Growable(ref mut c, ref mut len) => {
-                let bytes_written = c.write(buf)?;
-                *len += bytes_written;
-                bytes_written
-            },
-            StreamWriteBuffer::Sized(ref mut c, ref mut len) => {
-                let bytes_written = c.write(buf)?;
-                *len += bytes_written;
-                bytes_written
-            },
-        })
+        match *self {
+            StreamWriteBuffer::Growable(ref mut c) => c.write(buf),
+            StreamWriteBuffer::Sized(ref mut c) => c.write(buf),
+        }
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -81,17 +54,8 @@ impl<'a> Write for StreamWriteBuffer<'a> {
 impl<'a> AsRef<[u8]> for StreamWriteBuffer<'a> {
     fn as_ref(&self) -> &[u8] {
         match *self {
-            StreamWriteBuffer::Growable(ref c, ref len) => &c.get_ref()[0..*len],
-            StreamWriteBuffer::Sized(ref c, ref len) => &c.get_ref()[0..*len],
-        }
-    }
-}
-
-impl<'a> AsMut<[u8]> for StreamWriteBuffer<'a> {
-    fn as_mut(&mut self) -> &mut [u8] {
-        match *self {
-            StreamWriteBuffer::Growable(ref mut c, _) => c.get_mut(),
-            StreamWriteBuffer::Sized(ref mut c, _) => c.get_mut(),
+            StreamWriteBuffer::Growable(ref c) => &c.get_ref()[0..c.position() as usize],
+            StreamWriteBuffer::Sized(ref c) => &c.get_ref()[0..c.position() as usize],
         }
     }
 }
