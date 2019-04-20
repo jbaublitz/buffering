5d907262947fb9d6b8256addbe0f634e99be7fe8
John Baublitz <john.m.baublitz@gmail.com>
2019-04-20 09:37:02 -0400

    Add documentation, rework CI job, add .at_end() method to copy module, and bump versions

--- src/copy.rs
+++ src/copy.rs
@@ -1,10 +1,21 @@
 use std::io::{self,Cursor,Read,Write};
 
+/// A stream reader that will allow piece-by-piece reading of a buffer
 pub struct StreamReadBuffer<T>(Cursor<T>);
 
 impl<T> StreamReadBuffer<T> where T: AsRef<[u8]> {
+    /// Create a new reader with an underlying data type that can be expresssed as a byte slice
     pub fn new(buf: T) -> Self {
         StreamReadBuffer(Cursor::new(buf))
+    }
+
+    /// Check whether the stream has reached the end of the underlying buffer
+    pub fn at_end(&self) -> bool {
+        match self {
+            StreamReadBuffer(c) => {
+                c.position() == c.get_ref().as_ref().len() as u64
+            }
+        }
     }
 }
 
@@ -20,12 +31,16 @@ impl<T> AsRef<[u8]> for StreamReadBuffer<T> where T: AsRef<[u8]> {
     }
 }
 
+/// A stream writer that will allow piece-by-piece writing of to a buffer
 pub enum StreamWriteBuffer<'a> {
+    /// A wrapper around a byte vector-based cursor
     Growable(Cursor<Vec<u8>>),
+    /// A wrapper around a mutable slice-based cursor
     Sized(Cursor<&'a mut [u8]>),
 }
 
 impl<'a> StreamWriteBuffer<'a> {
+    /// Create a new vector-based stream writer that can grow when written past buffer boundaries
     pub fn new_growable(size: Option<usize>) -> Self {
         match size {
             Some(sz) => StreamWriteBuffer::Growable(Cursor::new(vec![0; sz])),
@@ -33,6 +48,7 @@ impl<'a> StreamWriteBuffer<'a> {
         }
     }
 
+    /// Create a new slice-based stream writer that will error when written past buffer boundaries
     pub fn new_sized(buf: &'a mut [u8]) -> Self {
         StreamWriteBuffer::Sized(Cursor::new(buf))
     }
@@ -57,5 +73,23 @@ impl<'a> AsRef<[u8]> for StreamWriteBuffer<'a> {
             StreamWriteBuffer::Growable(ref c) => &c.get_ref()[0..c.position() as usize],
             StreamWriteBuffer::Sized(ref c) => &c.get_ref()[0..c.position() as usize],
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_at_end_method() {
+        let mut b = StreamReadBuffer::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        let first_second_read = &mut [0u8; 4];
+        b.read(first_second_read).unwrap();
+        assert_eq!(b.at_end(), false);
+        b.read(first_second_read).unwrap();
+        assert_eq!(b.at_end(), false);
+        let last_read = &mut [0u8; 2];
+        b.read(last_read).unwrap();
+        assert_eq!(b.at_end(), true);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,42 @@
+#![deny(missing_docs)]
 #![feature(custom_attribute)]
 
+//! # Approach
+//! This crate is meant to provide two methods for serializing and deserializing with buffer
+//! operations:
+//! * The `copy` module is a more flexible, high-level stream based approach for reading from and
+//! writing to buffers. This will introduce some overhead so do not use this if copies are a bottleneck.
+//! * The `nocopy` module is a more restrictive macro-based approach. It uses procedural macros and
+//! unions to provide some level of safety when writing to fields in a struct while allowing the
+//! underlying struct to be interpreted as a slice. This is a C-like workflow but does provide
+//! some helpful guarantees that come with Rust like protection against buffer overflows and
+//! bounds checking. One very important note is the notion that structs that use the provided
+//! procedural macro must be completely allocatable on the stack. Compilation will fail if certain
+//! constructs that prevent size computation at compile time are used. As a result this really
+//! should only be used as an optimization for a more type safe common C workflow in very low level
+//! scenarios.
+//!
+//! # Features
+//! Each module is feature-flagged at build time to avoid pulling in unnecessary code if only one is
+//! necessary.  Available features are:
+//! * `copy`
+//! * `nocopy`
+
+/// A more restrictive macro-based approach to serializing and deserializing into buffers with some
+/// unsafe code to remove copy overhead
 #[cfg(feature = "nocopy")]
 #[macro_use]
 #[allow(unused_imports)]
 extern crate buffering_nocopy_macro;
 
+/// A more flexible way to serialize and deserialize into buffers that will have some copy overhead
 #[cfg(feature = "copy")]
 pub mod copy;
 
 #[cfg(feature = "copy")]
 pub use copy::*;
 
-#[cfg(test)]
+#[cfg(all(test, nocopy))]
 mod test {
     use std::mem;
 
